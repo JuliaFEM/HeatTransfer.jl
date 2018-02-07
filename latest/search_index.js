541a7ce0228ac7997cb6285b780054d8168fc8cb
autodocs <autodocs>
2018-02-07 04:44:21 +0000

    build based on 34bd642

---
+++ latest/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "This package extends JuliaFEM functionalities to solve heat transfer problems."
+},
+
+{
+    "location": "index.html#Theory-1",
+    "page": "Introduction",
+    "title": "Theory",
+    "category": "section",
+    "text": "From (Wikipedia)[https://en.wikipedia.org/wiki/Heat_equation]:The state equation, given by the first law of thermodynamics (i.e. conservation of energy), is written in the following form (assuming no mass transfer or radiation). This form is more general and particularly useful to recognize which property (e.g. c_p or rho) influences which term.rho c_pfrac partial Tpartial t-nabla cdot left(knabla Tright)=dot q_Vwhere dotq_V is the volumetric heat source."
+},
+
+{
+    "location": "index.html#Examples-1",
+    "page": "Introduction",
+    "title": "Examples",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Academic-examples-1",
+    "page": "Introduction",
+    "title": "Academic examples",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#References-1",
+    "page": "Introduction",
+    "title": "References",
+    "category": "section",
+    "text": "Heat equation. (2018, January 5). In Wikipedia, The Free Encyclopedia. Retrieved 00:49, January 30, 2018, from https://en.wikipedia.org/w/index.php?title=Heat_equation&oldid=818847673\nHeat transfer. (2018, January 26). In Wikipedia, The Free Encyclopedia. Retrieved 00:48, January 30, 2018, from https://en.wikipedia.org/w/index.php?title=Heat_transfer&oldid=822415173"
+},
+
+]}
