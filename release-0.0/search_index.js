f5a9d54dd1918a6b04897bb49469b57840e1b287
autodocs <autodocs>
2018-02-07 18:00:53 +0000

    build based on 699e321

---
+++ release-0.0/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "HeatTransfer.jl",
+    "title": "HeatTransfer.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#HeatTransfer.jl-1",
+    "page": "HeatTransfer.jl",
+    "title": "HeatTransfer.jl",
+    "category": "section",
+    "text": "HeatTransfer.jl extends JuliaFEM functionalities to solve heat transfer problems."
+},
+
+{
+    "location": "index.html#Theory-1",
+    "page": "HeatTransfer.jl",
+    "title": "Theory",
+    "category": "section",
+    "text": "The heat equation is a parabolic partial differential equation that describes the distribution of heat (or variation in temperature) in a given region over time. The state equation, given by the first law of thermodynamics (i.e. conservation of energy), is written in the following form (assuming no mass transfer or radiation). This form is more general and particularly useful to recognize which property (e.g. c_p or rho) influences which term. State equations isrho c_pfracpartial Tpartial t-nablacdotleft(knabla Tright)=dotq_Vwhere dotq_V is the volumetric heat source."
+},
+
+{
+    "location": "index.html#Features-1",
+    "page": "HeatTransfer.jl",
+    "title": "Features",
+    "category": "section",
+    "text": "PlaneHeat. Thermal conductivity k can be set using field thermal conductivity. Volumetric heat source dotq_V can be set using field heat source. Heat flux for boundary can be set using field heat flux. "
+},
+
+{
+    "location": "index.html#References-1",
+    "page": "HeatTransfer.jl",
+    "title": "References",
+    "category": "section",
+    "text": "Heat equation. (2018, January 5). In Wikipedia, The Free Encyclopedia. Retrieved 00:49, January 30, 2018, from https://en.wikipedia.org/w/index.php?title=Heat_equation&oldid=818847673\nHeat transfer. (2018, January 26). In Wikipedia, The Free Encyclopedia. Retrieved 00:48, January 30, 2018, from https://en.wikipedia.org/w/index.php?title=Heat_transfer&oldid=822415173"
+},
+
+]}
